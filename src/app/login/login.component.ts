05862c817845aacfa29a60601b735664d4017c7b
svaani <vani.shivanand@gmail.com>
2017-12-07 17:25:52 +0530

    login +routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,40 +21,16 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   login(usercreds) {
-
-    let userlogin = this.login2(usercreds);
-    //  userlogin.then((res) => {
-    //    if (res)
-    //    this.router.navigate(['/triplist']);
-    //  })
-  }
-
-  login2(usercreds) {
-    var headers = new Headers();
-    var creds = 'name=' + usercreds.username + '&password=' + usercreds.password + '&grant_type=password';
-    let client_id = '"my-trusted-client"';
-    let client_secret = 'secret';
-    var basicheader = btoa(client_id + ':' + client_secret);
-    var headersHere = new HttpHeaders();
-    headersHere.set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
-    headersHere.set('Authorization', 'Basic '+basicheader);
-    headersHere.set('Credentials','true');
-
-    var httpOptions = {
-      headers: headersHere
-    };
     
-    let myParams = { username: 'transity.ts@gmail.com',password : '123',grant_type:'password',client_id:'my-trusted-client' };
     let urlSearchParams = new URLSearchParams();
-    urlSearchParams.append('username', 'transity.ts@gmail.com');
-    urlSearchParams.append('password', '123');
+    urlSearchParams.append('username', this.user.username);
+    urlSearchParams.append('password', this.user.password);
     urlSearchParams.append('grant_type','password');
     urlSearchParams.append('client_id','my-trusted-client');
 
     this.http.post('http://localhost:8080/cargo/oauth/token',
-    urlSearchParams.toString(),
-      httpOptions).subscribe((data) => {
-
+    urlSearchParams.toString()).subscribe((data) => {
+      this.router.navigate(['/triplist']);
       }
       )
 
