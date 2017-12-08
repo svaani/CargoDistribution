2d0025396893a29b434cffca81b84e2d2c984b90
ragul <rkragul94@gmail.com>
2017-12-08 15:47:16 +0530

    google map and tab
    this is onely google map and tab

--- src/app/triplist/triplist.component.ts
+++ src/app/triplist/triplist.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,ElementRef, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth.service';
 
+declare var google: any;
 
 @Component({
   selector: 'app-triplist',
@@ -14,11 +15,35 @@ import {AuthService} from '../auth.service';
 
 export class TriplistComponent implements OnInit {
 
+  @ViewChild('map') mapRef:ElementRef;
+
   tripName: string = "hello";
 
   constructor(private http: HttpClient,private router: Router,private auth: AuthService) { }
 
   ngOnInit(): void {
+
+    this.DisplayMap();
   }
 
+  DisplayMap(){
+    
+         const location = new google.maps.LatLng(13.0005618,80.2478447);
+    
+         const options = {
+            center:location,
+            zoom:10,
+            streetViewControl:false,
+           
+          };
+            const map = new google.maps.Map(this.mapRef.nativeElement,options);
+          this.addMarker(location,map);
+      }
+    
+      addMarker(position,map) {
+          return new google.maps.Marker({
+            position,
+            map
+          });
+        }
 }
